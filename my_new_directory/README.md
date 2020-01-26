a27c9cf30d3b491fd576d462d83333bcfac04edc
Laura Archambault <laarchambault@gmail.com>
2020-01-26 01:35:16 +0000

    Automatically backed up by Learn

---
+++ my_new_directory/README.md
@@ -0,0 +1,5 @@
+Laura Archambault
+
+Some test text to see if it adds on new-branch-name but not master.
+
+This is my readme file
---
+++ while_variations.rb
@@ -0,0 +1,9 @@
+4.times do
+  puts "Good morning, Laura"
+end
+
+counter = 0 
+until counter == 20
+  puts "The current number is less than 20."
+  counter += 1 
+end
